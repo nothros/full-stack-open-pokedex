fa927b4e79273c1f2efe619d690e71786a22d79b
Erika Horros-Wallin <erika.horros@gmail.com>
2022-08-20 19:58:55 +0300

    add one more test to e2e

--- cypress/e2e/1-getting-started/e2etest.cy.js
+++ cypress/e2e/1-getting-started/e2etest.cy.js
@@ -17,4 +17,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
